7c4b2569c06faa9157acfbdc27f1deaac5ff645d
Tembero <silver.teemu@gmail.com>
2023-01-19 14:40:01 +0200

    Bug Fix & Reposive settings menu

--- web-service/components/Layout.tsx
+++ web-service/components/Layout.tsx
@@ -21,6 +21,12 @@ export default function Layout(props: { children: React.ReactNode }) {
     setSelectedTheme(theme);
     setCurrentAppTheme(theme);
 
+    // Check the os color scheme
+    const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
+    const osTheme = matches ? Theme.Dark : Theme.Light;
+    setCurrentOsTheme(osTheme);
+
+    // Listen for changes
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
       const osTheme = event.matches ? Theme.Dark : Theme.Light;
       setCurrentOsTheme(osTheme);
@@ -35,7 +41,7 @@ export default function Layout(props: { children: React.ReactNode }) {
         setSettingsOpen(openState);
       }, SETTINGS_ANIMATION_LENGTH);
       setSettingsDisplay(openState);
-    }else {
+    } else {
       setTimeout(() => {
         setSettingsDisplay(openState);
       }, SETTINGS_ANIMATION_LENGTH);
@@ -59,10 +65,16 @@ export default function Layout(props: { children: React.ReactNode }) {
 
     changeSettingsState(false);
   }
+  function onSettingsCancel() {
+    // Reset all settings here to the original state
+    setSelectedTheme(currentAppTheme);
+
+    changeSettingsState(false);
+  }
 
   return (
     <>
-      <ThemeProvider key={currentAppTheme} theme={currentAppTheme}/>
+      <ThemeProvider key={currentAppTheme} theme={currentAppTheme} />
       <div className={styles.container}>
         <div className={styles.navbar}>
           <Link href="/" className={styles.logoLink}><h1 className={styles.logo}>Safka.<br />Online</h1></Link>
@@ -76,40 +88,43 @@ export default function Layout(props: { children: React.ReactNode }) {
 
       <div className={styles.preferencesContainer} onClick={e => {
         if (e.target === e.currentTarget) {
-        changeSettingsState(!isSettingsOpen)
+          onSettingsCancel();
         }
       }} style={{
         display: isSettingsDisplay ? "flex" : "none",
         opacity: isSettingsOpen ? 1 : 0
       }}>
-          <div className={styles.preferences} style={{transform: `scale(${isSettingsOpen ? 1 : 0})`}}>
-              <h1>Käyttäjän Asetukset</h1>
-              <form onSubmit={onSettingsSubmit}>
-                <h2>Teemat</h2>
-                <div className={styles.themeSelect}>
-                  <label className={styles.themeSelectBlock}>
-                    <span>Järjestelmän Oletus <span className={styles.defaultTheme}>({currentOsTheme == Theme.Light ? "Vaalea" : "Tumma"})</span></span>
-                    <input type="radio" value={Theme.Default} checked={selectedTheme == Theme.Default} onChange={onThemeChanged} />
-                  </label>
-                  <label className={styles.themeSelectBlock}>
-                    Vaalea Teema
-                    <input type="radio" value={Theme.Light} checked={selectedTheme == Theme.Light} onChange={onThemeChanged} />
-                  </label>
-                  <label className={styles.themeSelectBlock}>
-                    Tumma Teema
-                    <input type="radio" value={Theme.Dark} checked={selectedTheme == Theme.Dark} onChange={onThemeChanged} />
-                  </label>
-                </div>
-                {/* <h2>Erityisruokavaliot</h2>
+        <form onSubmit={onSettingsSubmit} className={styles.preferences} style={{ transform: `scale(${isSettingsOpen ? 1 : 0})` }}>
+          <div className={styles.preferencesContent}>
+            <h1>Käyttäjän Asetukset</h1>
+            <h2>Teemat</h2>
+            <div className={styles.themeSelect}>
+              <label className={styles.themeSelectBlock}>
+                <span>Järjestelmän Oletus <span className={styles.defaultTheme}>({currentOsTheme == Theme.Light ? "Vaalea" : "Tumma"})</span></span>
+                <input type="radio" value={Theme.Default} checked={selectedTheme == Theme.Default} onChange={onThemeChanged} />
+              </label>
+              <label className={styles.themeSelectBlock}>
+                Vaalea Teema
+                <input type="radio" value={Theme.Light} checked={selectedTheme == Theme.Light} onChange={onThemeChanged} />
+              </label>
+              <label className={styles.themeSelectBlock}>
+                Tumma Teema
+                <input type="radio" value={Theme.Dark} checked={selectedTheme == Theme.Dark} onChange={onThemeChanged} />
+              </label>
+            </div>
+            {/* <h2>Erityisruokavaliot</h2>
                 <p>Näät vain ruuat joilla nämä erityisruokavaliot</p>
                 <div className={styles.dietChips}>
                   <DietChip>Laktoositon</DietChip>
                   <DietChip>Gluteeniton</DietChip>
                   <DietChip>Maidoton</DietChip>
                 </div> */}
-                <input className={styles.preferencesSubmit} type="submit" value="Tallenna" />
-              </form>
           </div>
+          <div className={styles.preferencesBottomBar}>
+            <input className={styles.preferencesBottomBtn} onClick={onSettingsCancel} type="button" value="Peruuta" />
+            <input className={styles.preferencesBottomBtn} type="submit" value="Tallenna" />
+          </div>
+        </form>
       </div>
 
     </>
@@ -119,17 +134,17 @@ export default function Layout(props: { children: React.ReactNode }) {
 function Footer() {
   return (
     <footer className={styles.footer}>
-        <h3 className={styles.footerHeader}>Resources</h3>
-        <ul className={styles.footerLinks}>
-            <li><FooterLink href="https://api.safka.online/v1/menu">API</FooterLink></li>
-            <li><FooterLink href="https://www.turkuai.fi/turun-ammatti-instituutti/opiskelijalle/ruokailu-ja-ruokalistat/ruokalista-juhannuskukkula-topseli">Data Source</FooterLink></li>
-            
-        </ul>
-        <h3 className={styles.footerHeader}>Open Source</h3>
-        <ul className={styles.footerLinks}>
-          <li><FooterLink href="https://github.com/Tembero11/Safka/tree/main/api-service">API</FooterLink></li>
-          <li><FooterLink href="https://github.com/Tembero11/Safka/tree/main/web-service">Web</FooterLink></li>
-        </ul>
+      <h3 className={styles.footerHeader}>Resources</h3>
+      <ul className={styles.footerLinks}>
+        <li><FooterLink href="https://api.safka.online/v1/menu">API</FooterLink></li>
+        <li><FooterLink href="https://www.turkuai.fi/turun-ammatti-instituutti/opiskelijalle/ruokailu-ja-ruokalistat/ruokalista-juhannuskukkula-topseli">Data Source</FooterLink></li>
+
+      </ul>
+      <h3 className={styles.footerHeader}>Open Source</h3>
+      <ul className={styles.footerLinks}>
+        <li><FooterLink href="https://github.com/Tembero11/Safka/tree/main/api-service">API</FooterLink></li>
+        <li><FooterLink href="https://github.com/Tembero11/Safka/tree/main/web-service">Web</FooterLink></li>
+      </ul>
     </footer>
   );
 }

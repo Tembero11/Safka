6185fc6d16535e6b99b834f9cf746a1165c033e5
Finnbyte <eelpy@protonmail.com>
2023-02-13 09:58:24 +0200

    Add fallback to v1 if error

--- api-service/src/api/startServer.ts
+++ api-service/src/api/startServer.ts
@@ -1,5 +1,4 @@
  import express from "express";
-import { archiver } from "..";
 import { apiResponse } from "./apiResponse";
 import {default as v1} from "./v1";
 import {default as v2} from "./v2";
@@ -10,12 +9,12 @@ app.disable("x-powered-by");
 
 interface StartServerOptions {
   apiBaseRoute?: string
+  revertToV1?: boolean 
 }
 
 export function startServer(port: number, options?: StartServerOptions) {
-  // Use apiv1 if database is up and running.
-  console.log(archiver);
-  archiver !== undefined ? app.use(`${options?.apiBaseRoute || "/"}v2`, v2) : app.use(`${options?.apiBaseRoute || "/"}v1`, v1);
+  // Use apiv2 if reverting not allowed. 
+  !options?.revertToV1 ? app.use(`${options?.apiBaseRoute || "/"}v2`, v2) : app.use(`${options?.apiBaseRoute || "/"}v1`, v1);
 
   app.get("*", function (req, res) {
     apiResponse(res, 404);

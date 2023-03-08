30758272a760923bb30618ecb810604a1709741e
Finnbyte <eelpy@protonmail.com>
2023-03-08 14:31:32 +0200

    Fix indent errors

--- web-service/pages/index.tsx
+++ web-service/pages/index.tsx
@@ -6,20 +6,20 @@ import getWeekMenu, { WeekMenu } from "../utils/getWeekMenu";
 
 const Home: NextPage<{ menu: WeekMenu | null }> = ({menu}) => {
   return (
-      <>
-        <Head>
-          <title>Safka Online</title>
-        </Head>
-        <p id="shortDesc">Juhannuskukkulan opiskelija- ja <br /> henkilöstöruokailun helposti luettava <br /> ruokalista netissä.</p>
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minHeight: "80%", gap: 20}}>
-          <Week menu={menu}/>
-          <p id="letterMeanings">
-            <span><Diet>L</Diet>&nbsp; Laktoositon</span>
-            <span><Diet>M</Diet>&nbsp; Maidoton</span>
-            <span><Diet>G</Diet>&nbsp; Gluteeniton</span>
-          </p>
-        </div>
-      </>
+    <>
+      <Head>
+        <title>Safka Online</title>
+      </Head>
+      <p id="shortDesc">Juhannuskukkulan opiskelija- ja <br /> henkilöstöruokailun helposti luettava <br /> ruokalista netissä.</p>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minHeight: "80%", gap: 20}}>
+        <Week menu={menu}/>
+        <p id="letterMeanings">
+          <span><Diet>L</Diet>&nbsp; Laktoositon</span>
+          <span><Diet>M</Diet>&nbsp; Maidoton</span>
+          <span><Diet>G</Diet>&nbsp; Gluteeniton</span>
+        </p>
+      </div>
+    </>
   );
 }
 

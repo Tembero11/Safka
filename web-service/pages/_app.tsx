d208459a1a9625b6dbe3bd72b4c82faf584a434b
Finnbyte <eelpy@protonmail.com>
2023-03-08 14:14:53 +0200

    Add code for sending data when on production

--- web-service/pages/_app.tsx
+++ web-service/pages/_app.tsx
@@ -1,12 +1,51 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Script from 'next/script'
+import * as gtag from "../utils/gtag"
+import { isProduction } from '../utils/common'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const isProd = isProduction()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!isProd) return
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      { isProd ? (<>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TAG}`}/>
+        <Script id="gtag-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.GA_TAG}', {
+page_path: window.location.pathname,
+});
+`,
+          }}
+        /></>) : null }
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }

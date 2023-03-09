65408736f44a6f8122d8e9119d4a3d946b9a5913
Tembero <silver.teemu@gmail.com>
2023-03-10 00:37:08 +0200

    Add sass support

--- web-service/next.config.js
+++ web-service/next.config.js
@@ -1,6 +1,19 @@
+const path = require('path');
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, "styles"),
+      path.join(__dirname, "components")
+    ]
+  }
 }
 
 module.exports = nextConfig

37e881b176991c79c62376ca199d0891be1a542d
Tembero <silver.teemu@gmail.com>
2023-03-10 01:20:31 +0200

    Make code compatible with new linting

--- web-service/components/DayBox.tsx
+++ web-service/components/DayBox.tsx
@@ -23,16 +23,16 @@ export default function DayBox(props: IProps) {
 
   return (
     <div className={styles.container + " " + (props.isToday ? styles.today : "")}>
-      <h2 className={styles.dayName}>{props.dayName}</h2>
+      <h2 className={styles["day-name"]}>{props.dayName}</h2>
       <div className={styles.break}></div>
       <div className={styles.box}>
         <p className={styles.date}>{date}</p>
-        <ul className={styles.menuList}>
+        <ul className={styles["menu-list"]}>
           {
             props.menu?.menu.map(food => {
               return (
                 <Fragment key={food.name}>
-                  <li className={styles.menuListItem}>
+                  <li className={styles["menu-list-item"]}>
                     {food.name}
                     {food.isLactoseFree ? <Diet longName="Laktoositon">L</Diet> : <></>}
                     {food.isDairyFree ? <Diet longName="Maidoton">M</Diet> : <></>}
@@ -55,7 +55,7 @@ export function Diet(props: { children: string, longName?: string }) {
   return (
     <span className={styles.diet} style={{cursor: props.longName ? "pointer" : "default"}}>
       {props.children}
-      {props.longName ? <div className={styles.dietTooltip}>{props.longName}</div> : <></>}
+      {props.longName ? <div className={styles["diet-tooltip"]}>{props.longName}</div> : <></>}
     </span>
   )
 }

55574f2b6b36fc26bc60345084a62b2b12f28c28
Finnbyte <eelpy@protonmail.com>
2023-03-14 13:52:32 +0200

    Remove g analytics and replace them with goatcounter alternatives

--- web-service/pages/_app.tsx
+++ web-service/pages/_app.tsx
@@ -2,46 +2,29 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
 import Script from 'next/script'
-import * as gtag from "../utils/gtag"
 import { isProduction } from '../utils/common'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 const isProd = isProduction()
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    if (!isProd) return
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      { isProd ? (<>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG}`}/>
-        <Script id="gtag-script"
+      { isProd && process.env.ANALYTICS_URL ? (<>
+        <Script id="goat-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', '${process.env.NEXT_PUBLIC_GA_TAG}', {
-page_path: window.location.pathname,
+window.goatcounter = {no_onload: true}
+window.addEventListener('hashchange', function(e) {
+window.goatcounter.count({
+path: location.pathname + location.search + location.hash,
+})
 });
-`,
-          }}
-        /></>) : null }
+`,}}
+        />
+        <script data-goatcounter={process.env.ANALYTICS_URL}
+          async src={`//${process.env.ANALYTICS_URL}/count.js`}></script>
+      </>) : null }
 
       <Layout>
         <Component {...pageProps} />

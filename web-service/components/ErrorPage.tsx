a8b1396fc821622faa23c8006e0f1fd495d3c511
ittuki <ittuki@kukkula.local>
2023-03-22 15:38:07 +0200

    Add key prop to fix bug

--- web-service/components/ErrorPage.tsx
+++ web-service/components/ErrorPage.tsx
@@ -14,7 +14,7 @@ export default function ErrorPage(props: IProps) {
       <h1 className={styles.header}>
         {props.code.toString().split("").map((char, index) => {
           return (
-            <span className={styles["err-digit"]} style={{ animationDelay: `${index * 100}ms` }}>{char}</span>
+            <span key={index} className={styles["err-digit"]} style={{ animationDelay: `${index * 100}ms` }}>{char}</span>
           )
         })}
         &nbsp;

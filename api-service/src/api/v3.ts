855e3413fb9bbeaf67d4fa69f354dfe22c8793e7
finnbyte <eelpy@protonmail.com>
2023-04-06 09:36:42 +0300

    Add working db fetch based on dayId

--- api-service/src/api/v3.ts
+++ api-service/src/api/v3.ts
@@ -1,40 +1,45 @@
 import cors from "cors";
 import { Router } from "express";
-import { getDayFromWeek } from "../foodUtils";
 import { currentMenu } from "../index";
 import { Weekday, WeekMenu } from "../types";
 import { db } from "../index";
 import { apiResponse } from "./apiResponse";
-import { addDays, subDays } from "date-fns";
+import { subDays } from "date-fns";
 
 const api = Router();
 api.use(cors());
 
 api.get("/v3/menu", async (req, res) => {
+  const foods = db.collection("foods")
   const wholeWeek: any = [];
-  await db.collection("foods").find({ week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } }).forEach(day => wholeWeek.push(day));
+  await foods
+    // Find using weekNumber AND year since week numbers are not year specific
+    .find({ week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } })
+    .forEach(day => wholeWeek.push(day));
 
   apiResponse(res, 200, { wholeWeek });
 });
 
 api.get("/v3/menu/today", async (req, res) => {
+  const foods = db.collection("foods")
   const today = new Date();
   const yesterday = subDays(today, 1);
 
   // Find dates between today and yesterday
   // Finds the current date
   // This method is used because finding exact ISO date string is really unreliable and annoying
-  const todaysMenu = await db.collection("foods").findOne({ date: { $gte: yesterday, $lt: today } });
+  const todaysMenu = await foods.findOne({ date: { $gte: yesterday, $lt: today } });
 
   apiResponse(res, 200, { ...todaysMenu });
 });
 
-api.get("/v3/menu/:dayId", (req, res) => {
+api.get("/v3/menu/:dayId", async (req, res) => {
+  const foods = db.collection("foods")
   const dayId = +req.params.dayId;
 
   if (Object.hasOwn(Weekday, dayId)) {
-    const day = getDayFromWeek(currentMenu, dayId);
-    apiResponse(res, 200, { ...day });
+    const menuOnDay = await foods.findOne({ dayId: dayId, week: { weekNumber: currentMenu.weekNumber } })
+    apiResponse(res, 200, { ...menuOnDay });
   } else {
     apiResponse(res, 400, { msg: "Invalid dayId" });
   }

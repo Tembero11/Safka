7fc8e04096371e7226b1e34d8140a23d293a079b
Finnbyte <eelpy@protonmail.com>
2023-05-18 15:05:11 +0300

    Add working middlewares from v2

--- api-service/src/api/v3.ts
+++ api-service/src/api/v3.ts
@@ -1,45 +1,44 @@
 import cors from "cors";
 import { Router } from "express";
-import { currentMenu } from "../index";
-import { Weekday, WeekMenu } from "../types";
+import { currentMenu, foods } from "../index";
+import { Weekday } from "../types";
 import { db } from "../index";
 import { apiResponse } from "./apiResponse";
 import { subDays } from "date-fns";
+import { DatabaseMenu } from "../database/dbTypes";
+import { convertFromDb } from "../database/db";
+import { getDayFromWeek } from "../foodUtils";
+import { getCurrentDayIndex } from "../utils";
 
 const api = Router();
 api.use(cors());
 
 api.get("/v3/menu", async (req, res) => {
-  const foods = db.collection("foods")
-  const wholeWeek: any = [];
-  await foods
+  const wholeWeek = await foods
     // Find using weekNumber AND year since week numbers are not year specific
-    .find({ week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } })
-    .forEach(day => wholeWeek.push(day));
+    .find({ week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } }).toArray() as DatabaseMenu[];
 
-  apiResponse(res, 200, { wholeWeek });
+  apiResponse(res, 200, { ...convertFromDb(wholeWeek) });
 });
 
 api.get("/v3/menu/today", async (req, res) => {
-  const foods = db.collection("foods")
   const today = new Date();
   const yesterday = subDays(today, 1);
 
   // Find dates between today and yesterday
   // Finds the current date
   // This method is used because finding exact ISO date string is really unreliable and annoying
-  const todaysMenu = await foods.findOne({ date: { $gte: yesterday, $lt: today } });
+  const todaysMenu = await foods.findOne({ date: { $gte: yesterday, $lt: today } }) as DatabaseMenu;
 
-  apiResponse(res, 200, { ...todaysMenu });
+  apiResponse(res, 200, { ...convertFromDb(todaysMenu).days[0] });
 });
 
 api.get("/v3/menu/:dayId", async (req, res) => {
-  const foods = db.collection("foods")
   const dayId = +req.params.dayId;
 
   if (Object.hasOwn(Weekday, dayId)) {
-    const menuOnDay = await foods.findOne({ dayId: dayId, week: { weekNumber: currentMenu.weekNumber } })
-    apiResponse(res, 200, { ...menuOnDay });
+    const menuOnDay = await foods.findOne({ dayId: dayId, week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } }) as DatabaseMenu;
+    apiResponse(res, 200, { ...convertFromDb(menuOnDay).days[0] });
   } else {
     apiResponse(res, 400, { msg: "Invalid dayId" });
   }

03e73424b5059b174bb42369d4c7a65ce36348da
Finnbyte <eelpy@protonmail.com>
2023-10-25 18:34:33 +0300

    Use today's index as a start for foods' slice operation

--- safka/src/routes/+page.server.ts
+++ safka/src/routes/+page.server.ts
@@ -1,8 +1,11 @@
+import { getTodaysIndex } from "$lib/utils";
 import { fetchFoods } from "../lib/fetchFoods";
 import { ApiUrl } from "../types";
 
 export async function load() {
+	const todayIndex = getTodaysIndex();
 	return {
-		foods: await fetchFoods(ApiUrl.v3)
+		foods: await fetchFoods(ApiUrl.v3, todayIndex),
+		todayIndex
 	}
 }

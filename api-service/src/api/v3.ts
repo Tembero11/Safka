4e6e62c642d90c0fc8d0cca5a4b19412b065823d
Finnbyte <eelpy@protonmail.com>
2023-10-25 20:40:24 +0300

    Implement restaurant validation and endpoint to fetch all defined restaurants

--- api-service/src/api/v3.ts
+++ api-service/src/api/v3.ts
@@ -10,30 +10,35 @@ import { archiver } from "../";
 import { getDayFromWeek } from "../foodUtils";
 import { getCurrentDayIndex } from "../utils";
 import { Archiver } from "../database/archiver";
+import restaurants from "../restaurants";
+import { validateRestaurantId } from "./middlewares/validation";
 
 const api = Router();
 api.use(cors());
 
-api.get("/v3/menu", async (req, res) => {
-  console.log(currentMenu)
+api.get("/v3/restaurants", async (req, res) => {
+  return apiResponse(res, 200, { restaurants });
+});
+
+api.get("/v3/menu/:restaurantId", validateRestaurantId, async (req, res) => {
   const week = await archiver.foods
     // Find using weekNumber AND year since week numbers are not year specific
     .find(
       { week: { weekNumber: currentMenu.weekNumber, year: new Date().getFullYear() } }, {sort: { version: -1}})
     .limit(7).toArray();
 
-  const payload = Archiver.fromDatabaseMenus(week)
+  const payload = Archiver.fromDatabaseMenus(week);
 
   return apiResponse(res, 200, { ...payload });
 });
 
-api.get("/v3/menu/today", async (req, res) => {
+api.get("/v3/menu/:restaurantId/today", validateRestaurantId, async (req, res) => {
   const today = new Date();
   const yesterday = subDays(today, 1);
 
   // Finds the current date
   // This method is used because finding exact ISO date string is really unreliable and annoying
-  const todaysMenu = await archiver.foods.findOne<DatabaseMenu>({ date: { $gte: yesterday, $lt: today } });
+  const todaysMenu = await archiver.foods.findOne<DatabaseMenu>({  date: { $gte: yesterday, $lt: today } });
   if (!todaysMenu) {
     return apiResponse(res, 500);
   }
@@ -43,7 +48,7 @@ api.get("/v3/menu/today", async (req, res) => {
   return apiResponse(res, 200, { ...payload });
 });
 
-api.get("/v3/menu/:dayId", async (req, res) => {
+api.get("/v3/menu/:restaurantId/:dayId", validateRestaurantId, async (req, res) => {
   const dayId = +req.params.dayId;
 
   if (!Object.hasOwn(Weekday, dayId)) 
@@ -54,7 +59,7 @@ api.get("/v3/menu/:dayId", async (req, res) => {
     return apiResponse(res, 500);
   }
 
-  const payload = Archiver.fromDatabaseMenu(menuOnDay)
+  const payload = Archiver.fromDatabaseMenu(menuOnDay);
 
   return apiResponse(res, 200, { ...payload });
 });

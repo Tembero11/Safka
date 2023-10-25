352240e275e81780b02abe2aea3517872c752c01
Tembero11 <silver.teemu@gmail.com>
2023-10-25 13:28:05 +0300

    Add svelte project

---
+++ safka/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<nav>
+    <h1>Safka <br/> Online.</h1>
+</nav>
+<main>
+    <slot/>
+</main>
+<footer>
+    <ul>
+        <li><a href="/">Source Code</a></li>
+        <li><a href="/">API</a></li>
+    </ul>
+</footer>
---
+++ safka/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<style>
+
+</style>
+
+<p>Hello fro msafka</p>

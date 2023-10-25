59a7ebe13f30b21e9e8e97560ae57f15e36a04d8
Finnbyte <eelpy@protonmail.com>
2023-10-25 15:19:44 +0300

    Add dots to sentences to make more conventional

--- safka/src/lib/fetchFoods.ts
+++ safka/src/lib/fetchFoods.ts
@@ -3,10 +3,10 @@ import type { WeekMenu } from "../types";
 /**
  * Fetches foods from an URL. Automatically slices off weekends, so days is 5-length and is 0 (monday) to 5 (friday).
  * Additional slicing can be controlled via `start` and `end` params.
- * @param url - URL of the API used for fetching
- * @param start - Start index of `days`. Example: `2` would start days off from Tuesday
- * @param start - End index of `days`. Example: `4` would cut days off from Thursday
- * @param end - URL of the API used for fetching
+ * @param url - URL of the API used for fetching.
+ * @param start - Start index of `days`. Example: `2` would start days off from Tuesday.
+ * @param start - End index of `days`. Example: `4` would cut days off from Thursday.
+ * @param end - URL of the API used for fetching.
  * @returns Week menu from API if fetch was succesful. Returns `null` if fetch wasn't succesful.
  */
 export async function fetchFoods(url: string, start?: number, end?: number): Promise<WeekMenu | null> {

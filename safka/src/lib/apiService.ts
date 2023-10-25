4d485812afbaa4e11e60ff50d27fdface721ebc8
Finnbyte <eelpy@protonmail.com>
2023-10-25 19:51:43 +0300

    Rename file name since added a new function

--- safka/src/lib/apiService.ts
+++ safka/src/lib/apiService.ts
@@ -1,15 +1,26 @@
-import type { WeekMenu } from "../types";
+import type { ApiUrl, IRestaurant, WeekMenu, restaurantId } from "../types";
+
+export async function fetchRestaurants(url: ApiUrl): Promise<IRestaurant[]> {
+    const res = await fetch(url);
+    if (!res.ok) {
+        // TODO: Add error handling
+        return [];
+    }
+
+    const restaurants: IRestaurant[] = (await res.json()).restaurants;
+    return restaurants;
+}
 
 /**
  * Fetches foods from an URL. Automatically slices off weekends, so days is 5-length and is 0 (monday) to 5 (friday).
  * Additional slicing can be controlled via `start` and `end` params.
  * @param url - URL of the API used for fetching.
- * @param start - Start index of `days`. Example: `2` would start days off from Tuesday.
- * @param start - End index of `days`. Example: `4` would cut days off from Thursday.
- * @param end - URL of the API used for fetching.
+ * @param restaurant - ID of restaurant where fetch() fetches the foods
+ * @param [start] - End index of `days`. Example: `4` would cut days off from Thursday.
+ * @param [end] - URL of the API used for fetching.
  * @returns Week menu from API if fetch was succesful. Returns `null` if fetch wasn't succesful.
  */
-export async function fetchFoods(url: string, start?: number, end?: number): Promise<WeekMenu | null> {
+export async function fetchFoods(url: ApiUrl, restaurant: restaurantId, start?: number, end?: number): Promise<WeekMenu | null> {
     const res = await fetch(url);
     if (!res.ok) {
         return null

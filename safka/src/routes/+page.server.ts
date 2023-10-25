88fa0a21c5bfd2e397925567b955e9e4474898d9
Finnbyte <eelpy@protonmail.com>
2023-10-25 19:54:43 +0300

    Add cookie to restaurantId parse logic

--- safka/src/routes/+page.server.ts
+++ safka/src/routes/+page.server.ts
@@ -1,11 +1,22 @@
 import { getTodaysIndex } from "$lib/utils";
-import { fetchFoods } from "../lib/fetchFoods";
-import { ApiUrl } from "../types";
+import { fetchFoods, fetchRestaurants } from "../lib/apiService";
+import { ApiUrl, restaurantIdSchema } from "../types";
+import type { PageServerLoadEvent } from "./$types";
 
-export async function load() {
+export async function load({ cookies }: PageServerLoadEvent) {
+	const restaurantCookie = cookies.get("restaurant")
+	
+	const idParseResult = restaurantIdSchema.safeParse(restaurantCookie);
+
+	const availableRestaurants = await fetchRestaurants(ApiUrl.v3_Restaurants);
+
+	const restaurant = idParseResult.success 
+		? availableRestaurants[idParseResult.data] // Indexable since it's guaranteed to have same index
+		: availableRestaurants[0]; // Default to first
+
 	const todayIndex = getTodaysIndex();
 	return {
-		foods: await fetchFoods(ApiUrl.v3, todayIndex),
+		foods: await fetchFoods(ApiUrl.v3_Menu, restaurant.id, todayIndex),
 		todayIndex
 	}
 }

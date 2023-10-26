3a4d30738f9d2c21756de6964971db5622e7c935
Finnbyte <eelpy@protonmail.com>
2023-10-26 11:58:08 +0300

    Add validation middleware for between route

--- api-service/src/api/middlewares/validation.ts
+++ api-service/src/api/middlewares/validation.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import restaurants from "../../restaurants";
 import { apiResponse } from "../apiResponse";
 import { NextFunction } from "express";
+import { parse, isValid, isAfter, differenceInBusinessDays } from "date-fns";
 
 const restaurantIdSchema = z.number().min(0).max(restaurants.length-1);
 
@@ -22,5 +23,28 @@ export function validateRestaurantId(req: Request, res: Response, next: NextFunc
   }
 
   res.locals.restaurantId = restaurantId;
+  next();
+}
+
+export function validateDateRange(req: Request, res: Response, next: NextFunction) {
+  console.log(req.params);
+  const startDate = parse(req.query.start as string, "dd.MM.yyyy", new Date());
+  const endDate = parse(req.query.end as string, "dd.MM.yyyy", new Date());
+
+  console.log("s", startDate, endDate);
+  if (!isValid(startDate) || !isValid(endDate)) {
+    return apiResponse(res, 400, { msg: "Invalid date given"});
+  }
+
+  if (isAfter(startDate, endDate)) {
+    return apiResponse(res, 400, { msg: "Invalid date range given (end should be later than start)"});
+  }
+
+  const durationInDays = differenceInBusinessDays(endDate, startDate);
+  if (durationInDays > 14) {
+    return apiResponse(res, 400, { msg: "Date range is over 14 business days"});
+  }
+
+  res.locals.dateRange = { startDate, endDate };
   next();
 }

6bd59973d81e58a11f45ed6f8fb6c6d94bf45d37
Finnbyte <eelpy@protonmail.com>
2023-10-27 23:11:49 +0300

    Add current prod implementation of determining which days to show and handling friday

--- safka/src/lib/apiService.ts
+++ safka/src/lib/apiService.ts
@@ -1,4 +1,6 @@
-import type { ApiUrl, IRestaurant, WeekMenu, restaurantId } from "../types";
+import type { ApiUrl, DayMenu, IRestaurant, restaurantId } from "../types";
+import { addBusinessDays } from "date-fns";
+import { formatDate } from "./utils";
 
 export async function fetchRestaurants(url: ApiUrl): Promise<IRestaurant[] | null> {
     try {
@@ -15,29 +17,31 @@ export async function fetchRestaurants(url: ApiUrl): Promise<IRestaurant[] | nul
     }
 }
 
+
 /**
  * Fetches foods from an URL. Automatically slices off weekends, so days is 5-length and is 0 (monday) to 5 (friday).
  * Additional slicing can be controlled via `start` and `end` params.
  * @param url - URL of the API used for fetching.
  * @param restaurant - ID of restaurant where fetch() fetches the foods
- * @param [start] - Start index of `days`. Example: `2` would start the array from Wednesday.
- * @param [end] - End index of `days`. Example: `4` would cut days off from Thursday.
  * @returns Week menu from API if fetch was succesful. Returns `null` if fetch wasn't succesful.
  */
-export async function fetchFoods(url: ApiUrl, restaurant: restaurantId, start?: number, end?: number): Promise<WeekMenu | null> {
+export async function fetchFoods(url: ApiUrl, restaurant: restaurantId): Promise<DayMenu[] | null> {
     try {
-        const res = await fetch(`${url}/${restaurant}`);
+        const today = new Date();
+        const startDate = formatDate(today);
+        const endDate = formatDate(addBusinessDays((today), 3));
+
+        const res = await fetch(`${url}/${restaurant}/between?start=${startDate}&end=${endDate}`);
         if (!res.ok) {
             return null
         }
 
-        const weekMenu: WeekMenu = await res.json()
-        const weekDays = weekMenu.days.slice(0, 5) // Slice away weekends, because they have no meals
+        const days: DayMenu[] = (await res.json()).days;
+        const weekDays = days.filter(day => day.hash); // Filter away weekends, because they have no meals
 
-        if (!start && !end) return { ...weekMenu, days: weekDays };
-
-        return { ...weekMenu, days: weekDays.slice(start ?? 0, end ?? weekDays.length) }
+        return weekDays;
     } catch (err) {
+        console.error(err)
         return null;
     }
 }

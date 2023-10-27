37710798e0f617e599eeb8b6d7237b6090c06828
Finnbyte <eelpy@protonmail.com>
2023-10-28 00:09:34 +0300

    Make error handling more apparent and conventional

--- safka/src/routes/+page.server.ts
+++ safka/src/routes/+page.server.ts
@@ -1,9 +1,15 @@
-import { getTodaysIndex } from "$lib/utils";
 import { fetchFoods, fetchRestaurants } from "../lib/apiService";
-import { ApiUrl, restaurantIdSchema } from "../types";
+import { ApiUrl, restaurantIdSchema, type DayMenu, type IRestaurant } from "../types";
+import { error } from "@sveltejs/kit";
 import type { PageServerLoadEvent } from "./$types";
 
-export async function load({ cookies }: PageServerLoadEvent) {
+interface ServerLoadResult {
+	foods: DayMenu[] | null;
+	restaurant: IRestaurant;
+	availableRestaurants: IRestaurant[];
+}
+
+export async function load({ cookies }: PageServerLoadEvent): Promise<ServerLoadResult> {
 	const restaurantCookie = Number(cookies.get("restaurant"))
 	
 	const idParseResult = restaurantIdSchema.safeParse(restaurantCookie);
@@ -12,18 +18,21 @@ export async function load({ cookies }: PageServerLoadEvent) {
 	// If we can't even get restaurants something is wrong.
 	// Just make foods null and effectively give up on showing data on the UI.
 	if (!availableRestaurants) {
-		return { foods: null }
+		throw error(500, "No available restaurants");
 	}
 
 	const restaurant = idParseResult.success 
 		? availableRestaurants[idParseResult.data] // Indexable since it's guaranteed to have same index
 		: availableRestaurants[0]; // Default to first
 
-	const todayIndex = getTodaysIndex();
+	const foods = await fetchFoods(ApiUrl.v3_Menu, restaurant.id);
+	if (!foods) {
+		throw error(404, "No menus");
+	}
+
 	return {
-		foods: await fetchFoods(ApiUrl.v3_Menu, restaurant.id),
+		foods,
 		restaurant,
 		availableRestaurants,
-		todayIndex
 	}
 }

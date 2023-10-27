45e61b604e34187fe84a6eceb91f02c9c044f0cb
Finnbyte <eelpy@protonmail.com>
2023-10-27 23:12:14 +0300

    Add utils function to format date to dd.MM.yyyy

--- safka/src/lib/utils.ts
+++ safka/src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 export function isProduction() {
     if (process.env.NODE_ENV !== "development") return true
     return false
@@ -6,3 +8,7 @@ export function isProduction() {
 export function getTodaysIndex() {
     return [6, 0, 1, 2, 3, 4, 5][new Date().getDay()];
 }
+
+export function formatDate(date: Date) {
+    return format(date, "dd.MM.yyyy")
+} 

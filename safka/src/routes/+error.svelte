25fae8e131d71ad01f14ec6fff82b6d5401c5c0b
Finnbyte <eelpy@protonmail.com>
2023-10-28 01:36:32 +0300

    Add simple error page

--- safka/src/routes/+error.svelte
+++ safka/src/routes/+error.svelte
@@ -1,1 +1,17 @@
-<p>oopsie</p>
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div id="page">
+    <h2>{$page.error?.message}</h2>
+</div>
+
+<style lang="scss">
+    #page {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content:  center;
+    }
+</style>

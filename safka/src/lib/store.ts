9642f9e775bd79fd8eb64f4178baa232d6aa9db8
Finnbyte <eelpy@protonmail.com>
2023-10-28 16:15:39 +0300

    Remove obsolete stores

--- safka/src/lib/store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-import type { DayMenu, IRestaurant } from "../types";
-
-export const restaurant = writable<IRestaurant>();
-export const foods = writable<DayMenu[]>();

61d4f4f84216d466784a603d5fc9f91b07296642
Finnbyte <eelpy@protonmail.com>
2023-10-29 16:21:30 +0200

    Create themes store

--- safka/src/lib/themes.ts
+++ safka/src/lib/themes.ts
@@ -7,4 +7,9 @@ export let theme: Writable<Theme>;
 if (browser) {
     const persistentTheme = localStorage.getItem("theme") 
     theme = writable<Theme>(persistentTheme as Theme ?? "os");
+    theme.subscribe(value => {
+        if (!browser) return
+        localStorage.setItem("theme", value);
+        document.documentElement.setAttribute("data-theme", value);
+    })
 }
